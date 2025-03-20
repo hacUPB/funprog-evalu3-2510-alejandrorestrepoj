09bef0c4eca9a8bb296414b862761d19e6a07ce9
alejandrorestrepoj <alejandro.restrepoj.col@upb.edu.co>
2025-03-20 09:14:29 -0500

    Correcciones Quiz.c
    Se agrego las 4 correcciones vistas en el quiz

--- Quiz/Quiz.c
+++ Quiz/Quiz.c
@@ -4,15 +4,15 @@ int main()
 {
     int stock, compras;
 
-        printf("Bienvenido al sistema de inventario. Stock disponible: 50\n");
+        printf("Bienvenido al sistema de inventario. Stock disponible: 50\n"); //Aqui se agrego \n
         stock = 50;
-        while (stock > 0)
+        while (stock > 0) //Se elimino el ";" que afectaba al codigo.
         {
             printf("Cuanto desea comprar: \n");
-            scanf("%d\n", &compras);
+            scanf("%d\n", &compras); //Se agrego ; 
             if (compras <= stock)
             {
-                stock = stock - compras;
+                stock = stock - compras; //Se agrego ;
                 printf("Compra realizada. Stock restante: %d\n", stock);
             }
             else
